8f23dccbb64cca5fa31944f191ab2d7ba4b779eb
kapolandia <paykapo4@gmail.com>
2024-03-27 16:44:39 +0100

    push milestone 1

--- main.js
+++ main.js
@@ -55,3 +55,41 @@ const posts = [
         "created": "2021-03-05"
     }
 ];
+
+posts.forEach(post => {
+    const {id, content, media, author, likes, created} = post;
+
+    document.getElementById('container').innerHTML += `
+    <div class="post">
+    <div class="post__header">
+        <div class="post-meta">                    
+            <div class="post-meta__icon">
+                <img class="profile-pic" src="${author.image}" alt="${author.name}">                    
+            </div>
+            <div class="post-meta__data">
+                <div class="post-meta__author">${author.name}</div>
+                <div class="post-meta__time">${created}</div>
+            </div>                    
+        </div>
+    </div>
+    <div class="post__text">${content}</div>
+    <div class="post__image">
+        <img src="${media}" alt="">
+    </div>
+    <div class="post__footer">
+        <div class="likes js-likes">
+            <div class="likes__cta">
+                <a class="like-button  js-like-button" href="#" data-postid="${id}">
+                    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+                    <span class="like-button__label">Mi Piace</span>
+                </a>
+            </div>
+            <div class="likes__counter">
+                Piace a <b id="like-counter-1" class="js-likes-counter">${likes}</b> persone
+            </div>
+        </div> 
+    </div>            
+</div>
+    ` 
+
+});
